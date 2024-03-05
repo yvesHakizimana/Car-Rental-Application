fe9371917b0e44f16ce4bde09a562421365a953f
Yves-HAKIZIMANA <hkzmnyves1@gmail.com>
2024-03-05 19:11:15 +0200

    User Model

---
+++ src/main/java/com/code/car/carrentalapp/entity/User.java
@@ -0,0 +1,29 @@
+package com.code.car.carrentalapp.entity;
+
+import com.code.car.carrentalapp.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String password;
+    @Enumerated
+    private UserRole userRole;
+}
