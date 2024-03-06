d7c12cfc21b1e1ece4b76736a350dc506337302a
Yves-HAKIZIMANA <hkzmnyves1@gmail.com>
2024-03-06 20:49:21 +0200

    AuthService Interface

--- src/main/java/com/code/car/carrentalapp/services/auth/AuthService.java
+++ src/main/java/com/code/car/carrentalapp/services/auth/AuthService.java
@@ -1,9 +1,13 @@
 package com.code.car.carrentalapp.services.auth;
 
+import com.code.car.carrentalapp.dto.AuthenticationRequest;
+import com.code.car.carrentalapp.dto.AuthenticationResponse;
 import com.code.car.carrentalapp.dto.SignupRequest;
 import com.code.car.carrentalapp.dto.UserDto;
 
 public interface AuthService  {
     UserDto createCustomer(SignupRequest signupRequest);
     boolean hasCustomerWithEmail(String email);
+    AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest);
+    void createAdminAccount();
 }
