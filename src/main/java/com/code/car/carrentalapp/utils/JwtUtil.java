b68e8d5f3cb618b6b9fe4938258b635d73a8c27d
Yves-HAKIZIMANA <hkzmnyves1@gmail.com>
2024-03-06 20:47:20 +0200

    Jwt Token Generation

--- src/main/java/com/code/car/carrentalapp/utils/JwtUtil.java
+++ src/main/java/com/code/car/carrentalapp/utils/JwtUtil.java
@@ -2,32 +2,29 @@ package com.code.car.carrentalapp.utils;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 import java.util.function.Function;
 
+@Component
 public class JwtUtil {
 
-    private static final String SECRET_KEY = "BWgwvfUQbvfyhCHdIlcrFDn8/dAlonrY+Kj+DJ8mhSkkFl2v+XDv/M1KK9jWO+Bi0PF88wwTmkciUO3x7/d9Mqo/lv+5qk/Fz/YM97ylIEXhAz/e/1iPC95+f3UoyiIxTYf5z2vRfKu6twhjUwMZ+9eKiLfxqkHpOqAmWZhPSr2TRL+V91I303uyoYy7cU8LiX0LVkvXvTDX4vS7wdvGPvLIUxBzC1I/Ko6n+5oHNZm+bQhoph0SEHP8ne9n6EZQ8/sMJNs8wLX7qyuanD8KhkAnn+vFTu4PTzNnV+JwUGN1L87qRiZ7CIoNwasazvgu0rvFZH5g3Hx2kVRM82zznCmbWgYdCCR0vpR1Ourj23M="
+    private static final String SECRET_KEY = "BWgwvfUQbvfyhCHdIlcrFDn8/dAlonrY+Kj+DJ8mhSkkFl2v+XDv/M1KK9jWO+Bi0PF88wwTmkciUO3x7/d9Mqo/lv+5qk/Fz/YM97ylIEXhAz/e/1iPC95+f3UoyiIxTYf5z2vRfKu6twhjUwMZ+9eKiLfxqkHpOqAmWZhPSr2TRL+V91I303uyoYy7cU8LiX0LVkvXvTDX4vS7wdvGPvLIUxBzC1I/Ko6n+5oHNZm+bQhoph0SEHP8ne9n6EZQ8/sMJNs8wLX7qyuanD8KhkAnn+vFTu4PTzNnV+JwUGN1L87qRiZ7CIoNwasazvgu0rvFZH5g3Hx2kVRM82zznCmbWgYdCCR0vpR1Ourj23M=";
 
-    public String extractUsername(String token){
-        return extractClaim(token, Claims::getSubject);
-    }
     public String generateToken(UserDetails userDetails){
-        return generateToken(new HashMap<>(), userDetails);
+        return createToken(new HashMap<>(), userDetails);
     }
 
     public boolean isTokenValid(String token, UserDetails userDetails){
-        final String userName = extractUsername(token);
-        return (userName.equals(userDetails.getUsername()) && !isTokenValid())
+        final String userName = getUsername(token);
+        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 
     private <T> T extractClaim(String token, Function<Claims, T> claimsResolvers){
@@ -35,7 +32,7 @@ public class JwtUtil {
         return claimsResolvers.apply(claims);
     }
 
-    private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails){
+    private String createToken(Map<String, Object> extraClaims, UserDetails userDetails){
         return Jwts
                 .builder()
                 .claims(extraClaims)
@@ -67,11 +64,13 @@ public class JwtUtil {
                 .getPayload();
     }
 
-
     public boolean isTokenExpired(String token){
         return extractExpiration(token).before(new Date());
     }
 
+    public String getUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
 
     private Date extractExpiration(String token){
         return extractClaim(token, Claims::getExpiration);
