1b10dd8de8715dc92b19e32510e33e56096b5cc0
Yves-HAKIZIMANA <hkzmnyves1@gmail.com>
2024-03-06 20:50:40 +0200

    AuthController

--- src/main/java/com/code/car/carrentalapp/controller/AuthController.java
+++ src/main/java/com/code/car/carrentalapp/controller/AuthController.java
@@ -1,27 +1,32 @@
 package com.code.car.carrentalapp.controller;
-
+import com.code.car.carrentalapp.dto.AuthenticationRequest;
+import com.code.car.carrentalapp.dto.AuthenticationResponse;
 import com.code.car.carrentalapp.dto.SignupRequest;
 import com.code.car.carrentalapp.dto.UserDto;
 import com.code.car.carrentalapp.services.auth.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
-
      private final AuthService authService;
 
+
+
      @PostMapping("/signup")
-     public ResponseEntity<?> signupCustomer(@RequestBody SignupRequest userCredentials){
+     public ResponseEntity<?> signUpCustomer(@RequestBody SignupRequest userCredentials){
          if(authService.hasCustomerWithEmail(userCredentials.getEmail()))
              return new ResponseEntity<>("Customer already exists with this email", HttpStatus.NOT_ACCEPTABLE);
          UserDto createdCustomerDto = authService.createCustomer(userCredentials);
@@ -29,5 +34,16 @@ public class AuthController {
              return new ResponseEntity<>("Customer not created", HttpStatus.BAD_REQUEST);
          return new ResponseEntity<>(createdCustomerDto, HttpStatus.CREATED);
      }
+
+     @PostMapping("/login")
+     public ResponseEntity<?> signInCustomer(@RequestBody AuthenticationRequest authRequest) throws BadCredentialsException, DisabledException, UsernameNotFoundException {
+         try{
+             AuthenticationResponse authResponse = authService.authenticate(authRequest);
+             return ResponseEntity.ok(authResponse);
+         } catch (RuntimeException ex){
+             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
+         }
+     }
 }
+
 
