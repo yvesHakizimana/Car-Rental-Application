88c6f43fb658515d297be01c8c9685ac6e0fe6f4
Yves-HAKIZIMANA <hkzmnyves1@gmail.com>
2024-03-06 20:56:18 +0200

    AdminService

---
+++ src/main/java/com/code/car/carrentalapp/services/admin/AdminService.java
@@ -0,0 +1,13 @@
+package com.code.car.carrentalapp.services.admin;
+
+import com.code.car.carrentalapp.dto.CarDto;
+import com.code.car.carrentalapp.entity.Car;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface AdminService {
+    boolean postCar(CarDto carDto) throws IOException;
+    List<CarDto> getAllCars();
+    CarDto deleteCar(Long id);
+}
